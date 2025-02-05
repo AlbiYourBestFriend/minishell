d5ba58c27403cb510b22867d864169997882e23b
Titouan Provost <tprovost@c1r6p5.42lehavre.fr>
2025-02-05 19:12:09 +0100

    modif

--- sources/utils/split_cmd_line.c
+++ sources/utils/split_cmd_line.c
@@ -6,7 +6,7 @@
 /*   By: tprovost <tprovost@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 19:03:18 by tprovost          #+#    #+#             */
-/*   Updated: 2025/02/04 19:20:39 by tprovost         ###   ########.fr       */
+/*   Updated: 2025/02/05 14:13:46 by tprovost         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,29 @@
 
 char	**split_cmd_line(char *cmd)
 {
-	int	i;
-	int	n;
-	int	c;
+	int		i;
+	int		j;
+	int		n;
+	int		c;
+	char	**tab;
 
 	i = 0;
 	n = 0;
 	while (cmd[i] != '\0')
 	{
 		c = is_in_str(cmd, i);
-		if (c == 0 && is_space(cmd[i]) == 0 && (is_space(cmd[i + 1]) == 1 || cmd[i + 1] == '\0'))
-		{
+		if (c == 0 && ft_isspace(cmd[i]) == 0 && (ft_isspace(cmd[i + 1]) == 1 || cmd[i + 1] == '\0'))
 			n++;
-		}
 		i++;
+	}
+	tab = malloc ((n + 1) * sizeof(char *));
+	i = 0;
+	while (cmd[i] != '\0')
+	{
+		j = i;
+		while (cmd[i] != '\0' && ft_isspace(cmd[i]) == 0)
+			i++;
+		if (cmd[i] != '\0')
+			i++;
 	}
 }
