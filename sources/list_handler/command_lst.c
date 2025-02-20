4431f7a833bca9ba10c2abd5060e1d141640356c
Marc-Olivier Leproux <mleproux@c1r6p6.42lehavre.fr>
2025-02-20 18:14:05 +0100

    fix ft_execute.c (encore), normisation du main

--- sources/list_handler/command_lst.c
+++ sources/list_handler/command_lst.c
@@ -6,7 +6,7 @@
 /*   By: mleproux <mleproux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 15:21:36 by mleproux          #+#    #+#             */
-/*   Updated: 2025/02/18 12:16:38 by mleproux         ###   ########.fr       */
+/*   Updated: 2025/02/20 15:21:35 by mleproux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ void	free_cmds(t_data *data)
 	temp = data->commands;
 	while (temp)
 	{
-		free_tab(temp->args);
+		if (temp->args)
+			free_tab(temp->args);
 		to_free = temp;
 		temp = temp->next;
 		to_free->next = NULL;
