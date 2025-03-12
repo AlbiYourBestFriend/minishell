0ed06b79a7891760b6082eb730c0a0b35e38313a
Marc-Olivier Leproux <mleproux@c1r6p6.42lehavre.fr>
2025-03-12 16:42:36 +0100

    h

---
+++ sources/utils/unlink_tmp.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unlink_tmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mleproux <mleproux@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 12:15:40 by mleproux          #+#    #+#             */
+/*   Updated: 2025/03/12 14:12:53 by mleproux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void	unlink_tmp(t_data *data)
+{
+	int		nbr;
+	char	*nbr_char;
+	char	*temp;
+
+	nbr = 1;
+	while (1)
+	{
+		nbr_char = ft_itoa(nbr);
+		if (!nbr_char)
+			return (allocate_error(data, ALLOC_ERR));
+		temp = ft_strjoin(data->tmp_path, nbr_char);
+		free(nbr_char);
+		if (!temp)
+			return (allocate_error(data, ALLOC_ERR));
+		if (access(temp, F_OK) == -1)
+			break;
+		else if (access(temp, W_OK) == 0)
+			unlink(temp);
+		free(temp);
+		nbr++;
+	}
+	free(temp);
+}
